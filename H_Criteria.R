9abb70d1556a71a9d141ea31848daee6cf2e06fd
xyan23 <159835995+xyan23@users.noreply.github.com>
2024-05-29 03:34:32 -0500

    Update H_Criteria.R

--- H_Criteria.R
+++ H_Criteria.R
@@ -1,142 +1,143 @@
-x<-c(1,8,5,7,4,2)
-y<-c(2,4,10,3,5,6)
-m<-matrix(c(x,y), nrow = length(x), ncol = 2)
-
-#HeapBottomUp with matrix
-HeapBottomUp_m<-function(M){
-  n<-nrow(M)
-  f<-floor(n/2)
-  M_c<-M
-  for (i in f:1){
-    k<-i
-    v<-M_c[i,]
-    heap<-FALSE
-    while (!heap && 2*k<=n){
-      j<-2*k
-      if (j < n){
-        if (M_c[j,][1]< M_c[(j+1),][1]){
-          j<-j+1
+#### Algorithm 1
+heap_bottom_up <- function(vec,comp_fun) {
+  n <- length(vec)
+  # Locate the parental nodes
+  f <- floor(n / 2)
+  for (i in f:1) {
+    # Index
+    k <- i
+    v <- vec[i]
+    heap <- FALSE
+    # Make sure this is a complete binary tree
+    while (!heap && 2 * k <= n) {
+      j <- 2 * k
+      # Left child index
+      if (j < n) {
+        # Compare between left and right child
+        if (comp_fun(vec[j], vec[j + 1])) {
+          j <- j + 1
         }
       }
-      if (v[1]>=M_c[j,][1]){
-        heap<-TRUE
-      }
-      else {
-        M_c[k,][1]<-M_c[j,][1]
-        k<-j
+      # Compare parent and child nodes
+      if (comp_fun(vec[j], v)) {
+        heap <- TRUE
+      } else {
+        # Swap the child with parent
+        vec[k] <- vec[j]
+        k <- j
       }
     }
-    M_c[k,]<-v
+    vec[k] <- v
+  }  
+  return(vec)
+} 
+
+heapsort <- function(vec, comp_fun) {
+  l <- length(vec)
+  # Create empty vector to restore ordered element 
+  sortvec <- numeric(length(vec))
+  i = 1
+  # Repeat for n-1 times
+  while (length(vec) > 1) {
+    # Heapify the vector
+    vec <- heap_bottom_up(vec,comp_fun)
+    n <- length(vec)
+    # Swap the root with the last element
+    v_n <- vec[n]
+    # Restore the root of each heap
+    sortvec[i] <- vec[1]
+    vec[1] <- v_n
+    # Delete the last element
+    vec <- vec[1:n-1]
+    i <- i+1
   }
-  order_h<-match(M_c[,1],M[,1])
-  H<-M[order_h,]
-  return(H)
-}    
+  sortvec[l] <- vec[1]
+  return(sortvec)
+}
 
-HeapBottomUp(m)
+####Example
+x<-c(2,9,7,6,5,8)
 
-compare_function_1 <- function(p1, p2) {
-  comparison_criterion <- p1[1] > p2[1]
+compare_function <- function(p1, p2) {
+  comparison_criterion <- p1 < p2
   if(comparison_criterion) {
-    return(p1)
+    TRUE
   } else {
-    retrun(p2)
-  }
-  
-  
-  v<-M_c[i,]
-  j<-2*i
-  if (j < n){
-    if (M_c[j,][1]< M_c[(j+1),][1]){
-      j<-j+1
-    }
-  }
-  if (v[1]>=M_c[j,][1]){
-    heap<-TRUE
-  }
-  else {
-    M_c[i,][1]<-M_c[j,][1]
-    i<-j
+    FALSE
   }
 }
 
-compare_function_2 <- function(p1, p2) {
-  comparison_criterion <- p1[2] > p2[2]
+heap_bottom_up(x,compare_function)
+heapsort(x,compare_function)
+
+#### Algorithm 2
+max_heapify <- function(A, i, comp_fun) {
+  heap_size <- length(A)
+  # Left child index
+  l <- 2 * i  
+  # Right child index
+  r <- 2 * i + 1         
+  # Make sure this is a complete binary tree
+  # Compare between root and left child
+  if (l <= heap_size && comp_fun(A[l],A[i])) {
+    largest <- l
+  } else {
+    largest <- i
+  }
+  # Compare between largest and left child
+  if (r <= heap_size && comp_fun(A[r],A[largest])) {
+    largest <- r
+  }
+  # If largest is not root
+  if (largest != i) {
+    # Swap the larger child with parent
+    key <- A[i]
+    A[i] <- A[largest]
+    A[largest] <- key
+    # Recursively heapify for sub-tree
+    A <- max_heapify(A, largest, comp_fun)
+  }
+  
+  return(A)
+}
+
+build_max_heap <- function(A, comp_fun) {
+  n <- length(A)
+  # Check all the parental nodes
+  for (i in floor(n / 2):1) {
+    A <- max_heapify(A, i, comp_fun)
+  }
+  return(A)
+}
+
+
+heapsort <- function(A, comp_fun) {
+  n <- length(A)
+  A <- build_max_heap(A, comp_fun)
+  for (i in n:2) {
+    # Exchange the first element with the ith element
+    key <- A[1]
+    A[1] <- A[i]
+    A[i] <- key
+    # Decrease the heap size by 1
+    A[1:(i-1)] <- max_heapify(A[1:(i-1)], 1, comp_fun)
+  }
+  return(A)
+}
+
+####Example
+x<-c(2,9,7,6,5,8)
+
+compare_function <- function(p1, p2) {
+  comparison_criterion <- p1 > p2
   if(comparison_criterion) {
-    return(p1)
+    TRUE
   } else {
-    retrun(p2)
-  }
-  
-  v<-M_c[i,]
-  j<-2*i
-  if (j < n){
-    if (M_c[j,][2]< M_c[(j+1),][2]){
-      j<-j+1
-    }
-  }
-  if (v[2]>=M_c[j,][2]){
-    heap<-TRUE
-  }
-  else {
-    M_c[i,][2]<-M_c[j,][2]
-    i<-j
+    FALSE
   }
 }
 
-
-HeapBottomUp_m<-function(M,comp_func){
-  n<-nrow(M)
-  f<-floor(n/2)
-  M_c<-M
-  for (i in f:1){
-    k<-i
-    v<-M_c[i,]
-    heap<-FALSE
-    while (!heap && 2*k<=n){
-      j<-2*k
-      if (j < n){
-        if (M_c[j,][1]< M_c[(j+1),][1]){
-          j<-j+1
-        }
-      }
-      if (v[1]>=M_c[j,][1]){
-        heap<-TRUE
-      }
-      else {
-        M_c[k,][1]<-M_c[j,][1]
-        k<-j
-      }
-    }
-    M_c[k,]<-v
-  }
-  order_h<-match(M_c[,1],M[,1])
-  H<-M[order_h,]
-  return(H)
-}    
-
-HeapBottomUp_c(m)
-
-Heapsort<-function(M,comp_func){
-  l1<-nrow(M)
-  l2<-ncol(M)
-  M_c = M
-  sortmatrix<-matrix(,nrow=l1,ncol=l2)
-  i=1
-  while (nrow(M_c) >1){
-    M_c<-HeapBottomUp(M_c)
-    n<-nrow(M_c)
-    #Last row
-    p_n<-M_c[n,]
-    sortmatrix[i,]<-M_c[1,]
-    #Exchange the first row and the last row
-    M_c[1,]<-p_n
-    M_c<-M_c[1:(n-1),] #argument is of length zero????
-    i<-i+1
-  }
-  sortmatrix[l1,]<-M_c[1,]
-  return(sortmatrix)
-}
-Heapsort(m)
+build_max_heap(x, compare_function)
+heapsort(x, compare_function)
 
 
