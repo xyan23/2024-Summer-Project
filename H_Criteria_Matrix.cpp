e71dc1c1586bd0257d31394ad074b24aec9a78bb
xyan23 <159835995+xyan23@users.noreply.github.com>
2024-06-07 14:34:58 -0500

    Update H_Criteria_Matrix.cpp

--- H_Criteria_Matrix.cpp
+++ H_Criteria_Matrix.cpp
@@ -1,60 +1,64 @@
 #include <Rcpp.h>
 using namespace Rcpp;
-using namespace std;
 
-void max_heapify_c(NumericMatrix A, int i, Function comp_fun) {
-  int heap_size = A.nrow();
+// [[Rcpp::export]]
+NumericMatrix max_heapify_c(NumericMatrix A, int i, int heap_size) {
+  A = clone(A);
   // Left child index
   int l = 2 * i + 1; 
   // Right child index
   int r = 2 * i + 2; 
   int largest; 
-  NumericVector key;       
+  NumericVector key;
+
   // Make sure this is a complete binary tree
   // Compare between root and left child
-  if (l <= heap_size && comp_fun(A[l],A[i])) {
+  if (l < heap_size && A(l, 0) > A(i, 0)) {
     largest = l;
   } else {
     largest = i;
   }
   // Compare between largest and left child
-  if (r <= heap_size && comp_fun(A[r],A[largest])) {
+  if (r < heap_size && A(r, 0) > A(largest, 0)) {
     largest = r;
   }
   // If largest is not root
   if (largest != i) {
     // Swap the larger child with parent
-    key = A[i];
-    A[i] = A[largest];
-    A[largest] = key;
+    key = A(i, _);
+    A(i, _) = A(largest, _);
+    A(largest, _) = key;
     // Recursively heapify for sub-tree
-    A = max_heapify_c(A, largest, comp_fun);
+    A = max_heapify_c(A, largest, heap_size);
   }
   
   return(A);
 }
 
-void build_max_heap_c(NumericMatrix A, Function comp_fun) {
-  int n = A.nrow();
+// [[Rcpp::export]]
+NumericMatrix build_max_heap_c(NumericMatrix A) {
+  A = clone(A);
+  int heap_size = A.nrow();
   // Check all the parental nodes
-  for (int i = n / 2 - 1; i >= 0; i--) {
-    A = max_heapify_c(A, i, comp_fun);
+  for (int i = floor(heap_size / 2); i >= 0; --i) {
+    A = max_heapify_c(A, i, heap_size);
   }
   return(A);
 }
 
-
-void heapsort_c(NumericMatrix A, Function comp_fun) {
+// [[Rcpp::export]]
+NumericMatrix heapsort_c(NumericMatrix A) {
+  A = clone(A);
   int n = A.nrow();
-  A = build_max_heap_c(A, comp_fun);
+  A = build_max_heap_c(A);
   NumericVector key;
-  for (int i = n - 1; i >= 1; i--) {
+  for (int i = n - 1; i >= 1; --i) {
     // Exchange the first element with the ith element
-    key = A[0];
-    A[0] = A[i];
-    A[i] = key;
-    // Decrease the heap size by 1
-    A[1:(i-1)] <- max_heapify_c(A[1:(i-1)], 1, comp_fun);
+    key = A(0, _);
+    A(0, _) = A(i, _);
+    A(i, _) = key;
+    // Decrease the heap size by setting the heap size to i
+    A = max_heapify_c(A, 0, i);
   }
   return(A);
 }
